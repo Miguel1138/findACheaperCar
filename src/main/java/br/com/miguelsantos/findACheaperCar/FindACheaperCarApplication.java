027ca0f67614dafe6bbb71781bb4c567a95f216e
Miguel <miguel.sdasilva00@gmail.com>
2025-04-08 19:14:37 -0300

    initial Commit

---
+++ src/main/java/br/com/miguelsantos/findACheaperCar/FindACheaperCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.miguelsantos.findACheaperCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindACheaperCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindACheaperCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/miguelsantos/findACheaperCar/FindACheaperCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguelsantos.findACheaperCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindACheaperCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
