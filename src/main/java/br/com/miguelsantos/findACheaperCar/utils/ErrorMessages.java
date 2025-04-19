97c4f408dc184f66f263fa6a1ec61acbce32daa1
Miguel <miguel.sdasilva00@gmail.com>
2025-04-19 15:08:44 -0300

    Add Error Messages
    - Centralize error messages in one class
    - More easy to track and maintain error handlers.

---
+++ src/main/java/br/com/miguelsantos/findACheaperCar/utils/ErrorMessages.java
@@ -0,0 +1,6 @@
+package br.com.miguelsantos.findACheaperCar.utils;
+
+public final class ErrorMessages {
+    public static final String DEFAULT_MESSAGE = "Valor inválido!";
+    public static final String TOO_MANY_TRIES = "Número máximo de tentativas excedidas, programa finalizado!";
+}
